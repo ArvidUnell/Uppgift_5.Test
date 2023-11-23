557665b856d906b6d746c52414cfb2f6541dd9ff
arun06001 <arun06001@E-PW0234KE>
2023-11-23 10:10:48 +0100

    Add project files.

---
+++ Uppgift_5.Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Uppgift_5_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namnArr = new string[5];
+            byte[] åldrar = new byte[namnArr.Length];
+
+            for (int i = 0; i < namnArr.Length; i++)
+            {
+                Console.WriteLine("Skriv in ett namn på en klasskompis");
+                namnArr[i] = Console.ReadLine();
+
+                Console.WriteLine($"Skriv in {namnArr[i]}s ålder");
+                while (true)
+                {
+                    try
+                    {
+                        åldrar[i] = byte.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Det är inte en giltig ålder. Försök igen");
+                    }
+                }
+            }
+
+            short ålderSumma = 0;
+
+            foreach (byte ålder in åldrar)
+            {
+                ålderSumma += ålder;
+            }
+
+            byte maxÅlder = åldrar.Max();
+            byte äldstIndex = (byte)Array.IndexOf(åldrar, maxÅlder);
+
+            Console.WriteLine($"Medelvärdet för åldrarna är {ålderSumma/åldrar.Length}");
+            Console.WriteLine($"Den äldsta personen är {namnArr[äldstIndex]} som är {maxÅlder} år gammal.");
+
+            Console.WriteLine("\nTryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
